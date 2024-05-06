aef7a11b5ff40aa76f88d36de403f40d9bed9086
voltarou <bagusadnan04@gmail.com>
2024-05-06 11:37:38 +0800

    pMhs

--- JawaKocak/JawaKocak.cpp
+++ JawaKocak/JawaKocak.cpp
@@ -10,13 +10,19 @@ public:
 };
 
 int main()
-{ 
+{
     mahasiswa mhs{ 1 };
     mhs.showNim();
 
     mahasiswa& refMhs = mhs;
     refMhs.nim = 2;
     mhs.showNim();
+
+
+    mahasiswa* pMhs = &mhs;
+    pMhs->nim = 3;
+    mhs.showNim();
+    return 0;
 }
 
 
