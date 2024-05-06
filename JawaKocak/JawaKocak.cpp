3093c0296ecdaaee485a109186e469c7675ad168
voltarou <bagusadnan04@gmail.com>
2024-05-06 11:30:54 +0800

    class public

--- JawaKocak/JawaKocak.cpp
+++ JawaKocak/JawaKocak.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+class mahasiswa {
+public:
+    int nim;
+    void shownim() {
+        cout << "No Induk = " << nim << endl;
+    }
+};
+
 int main()
 { 
     
